1be7d45bad086194281d8cd25eba386e40af002c
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-02-16 21:00:31 +0100

    asset pipeline #1

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,20 @@ module Ninayo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+     # via https://gist.github.com/afeld/5704079
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
