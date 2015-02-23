c9102bbb0fc4a73b1b4a2da6b422a9d53c8cc00b
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-02-23 07:20:00 +0100

    Changed map logic to work with turbolinks

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,28 @@
 (function($) {
+	'use strict'
 
-	var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
+	var map,
+		mapDiv;
 
-  var map = new google.maps.Map(document.getElementById('admap'),
-      mapOptions);
+	$(document).on('ready page:load', function() {
+
+	 	mapDiv = $('#admap');
+	 	if (!mapDiv.length) {
+	 		return;
+	 	}
+
+	 	if (typeof google != 'undefined' && google.maps) {
+			initMap();
+	 	}
+	});
+
+	var initMap = function() {
+		console.log('initMap')
+		map = new google.maps.Map(document.getElementById('admap'), {
+			zoom: 8,
+			center: new google.maps.LatLng(-34.397, 150.644)
+		});
+		isInitialized = true;
+	}
 
 })(jQuery);
--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,4 +1,5 @@
 (function($) {
+	'use strict'
 
 	$(document).on('click', '.siteheader-toggle', function(e) {
 		e.preventDefault();
