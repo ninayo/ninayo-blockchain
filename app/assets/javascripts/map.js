c645b07a06a18a96e5a10a8ebdf184540e642029
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-02-24 13:55:57 +0100

    sticky footer

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -19,8 +19,15 @@
 	var initMap = function() {
 		map = new google.maps.Map(document.getElementById('admap'), {
 			zoom: 8,
-			center: new google.maps.LatLng(-7.458111, 35.991116)
+			center: new google.maps.LatLng(-7.458111, 35.991116),
+			mapTypeId: google.maps.MapTypeId.TERRAIN
 		});
+
+		// google.maps.event.addDomListener(map, 'tilesloaded', function(){
+		// 	if($('.map-controls').length == 0){
+		// 		$('div.gmnoprint').last().parent().wrap('<div class="map-controls" />');
+		// 	}
+		// });
 		isInitialized = true;
 	}
 
