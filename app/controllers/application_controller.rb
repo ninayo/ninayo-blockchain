20ffacb53aa5d4a354f2a329c0e588640c4d9062
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-04-11 08:18:53 +0200

    Added setting for preferred language on edit profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,15 @@ class ApplicationController < ActionController::Base
 
 	def set_locale
 		# I18n.locale = :en
-		I18n.locale = params[:locale] || I18n.default_locale
+		if params[:locale]
+			I18n.locale = params[:locale]
+		elsif current_user
+			I18n.locale = current_user.language
+		else
+			I18n.default_locale
+		end
+
+		#I18n.locale = params[:locale] || I18n.default_locale
 		# I18n.locale = current_user.language if current_user
 	end
 
@@ -37,6 +45,6 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me, :name, :phonenumber, :region, :village, :language, :lat, :lng) }
 		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :phone_number, :region_id, :village, :language, :lat, :lng) }
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :phone_number, :region_id, :village, :language, :lat, :lng, :language) }
 	end
 end
