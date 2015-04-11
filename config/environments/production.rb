5b02006b4e80e85cbefdc142765bf44ad6b39175
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-04-11 08:59:40 +0200

    meh...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'www.ninayo.com' }
   Rails.application.routes.default_url_options[:host] = 'www.ninayo.com'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "587",
