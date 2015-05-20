61b80916476568bf7d15174e927ba45be3aa6dbb
Gabriel Svennerberg <gabriel@svennerberg.com>
2015-05-20 15:43:42 +0200

    Added module tabs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418091451) do
+ActiveRecord::Schema.define(version: 20150520090908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150418091451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name_sw"
+    t.boolean  "is_other"
   end
 
   create_table "event_types", force: :cascade do |t|
