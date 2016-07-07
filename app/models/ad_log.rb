3b4b75142f5616c12ed62ebc8e142a6b05bebb2d
beck <beck.nygard@gmail.com>
2016-07-07 14:10:17 -0700

    removed another injection vuln in adlog summary

--- app/models/ad_log.rb
+++ app/models/ad_log.rb
@@ -6,12 +6,14 @@ class AdLog < ActiveRecord::Base
 	def self.summary(ad_id = nil)
 		#sql = "select EXTRACT(year FROM start_time)::int AS year,  EXTRACT(month FROM start_time)::int AS month, count(id) from trips WHERE user_id = :user_id GROUP BY year, month ORDER BY year, month;"
 		#sql = "select ad_id, count(id) from ad_logs WHERE event_type_id = :event_type_id GROUP BY ad_id;"
-		if ad_id
-			sql = "select ad_id, event_type_id, count(id) AS count from ad_logs WHERE ad_id = #{ad_id} GROUP BY ad_id, event_type_id ORDER BY ad_id, event_type_id;"
-			find_by_sql(sql)
-		else
-			sql = "select ad_id, event_type_id, count(id) AS count from ad_logs GROUP BY ad_id, event_type_id ORDER BY ad_id, event_type_id;"
-			find_by_sql(sql)
-		end
+		
+		#removing this for the time being, since it isnt doing anything and is an injection vulnerability
+		# if ad_id
+		# 	sql = "select ad_id, event_type_id, count(id) AS count from ad_logs WHERE ad_id = #{ad_id} GROUP BY ad_id, event_type_id ORDER BY ad_id, event_type_id;"
+		# 	find_by_sql(sql)
+		# else
+		# 	sql = "select ad_id, event_type_id, count(id) AS count from ad_logs GROUP BY ad_id, event_type_id ORDER BY ad_id, event_type_id;"
+		# 	find_by_sql(sql)
+		# end
 	end
 end
