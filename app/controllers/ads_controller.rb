206c245d103f26e996c41291d65be4ff2f660154
beck <beck.nygard@gmail.com>
2016-07-12 14:51:15 -0700

    added map marker culling to improve performance, map shows markers for last 40 days

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -22,7 +22,7 @@ class AdsController < ApplicationController
 	end
 
 	def map
-		@ads = @ads.includes(:crop_type)
+		@ads = @ads.includes(:crop_type).where(:created_at => 40.days.ago..Time.now)
 		@crop_types = CropType.all.order(:sort_order)
 		@regions = Region.all.order(:name)
 	end
