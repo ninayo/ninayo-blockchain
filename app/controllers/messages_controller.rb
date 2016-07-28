9e5bf40db90d948f4c06288de190aa378f173904
beck <beck.nygard@gmail.com>
2016-07-28 12:05:51 -0700

    fixed a bug with user targeting on new message create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   #right now this only gets one user out of the params, but recipients can be modified to support multiple users
   #conversation subject params are hardcoded in the view right now but this setup will take arbitrary input
   def create
-    recipients = [User.find_by_id(params[:uid].keys.first)]
+    recipients = User.where(id: params['recipients']) || [User.find_by_id(params[:uid].keys.first)]
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message sent"
     redirect_to conversation_path(conversation)
