35e81677501a1305742f5616afa172fc72540f48
beck <beck.nygard@gmail.com>
2016-09-29 12:14:05 -0700

    minor update to bot posting

--- app/controllers/bot/bot_controller.rb
+++ app/controllers/bot/bot_controller.rb
@@ -71,9 +71,10 @@ class Bot::BotController < ApplicationController
     end
 
     @ad.user_id = @user.id
+    @ad.ad_type = 0
+    @ad.status = 1
 
     if @ad.save
-      @ad.update(:status => 1)
       ad_success
     else
       fail_message(@ad.errors.full_messages)
