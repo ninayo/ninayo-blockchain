a934cd2d4044c69a0d9bbc57811e4eac30600592
beck <beck.nygard@gmail.com>
2016-10-28 14:41:05 -0700

    wrong ad reference

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -38,7 +38,7 @@ class MessagesController < ApplicationController
   private
 
   def track_message
-    track_event("Engagement & Acquisition", "Site message", "in-site message sent on #{@ad.ad_type} ad: #{@ad.region.name}", "SEND MESSAGE TO AD")
+    track_event("Engagement & Acquisition", "Site message", "in-site message sent", "SEND MESSAGE TO AD")
   end
 
 end
