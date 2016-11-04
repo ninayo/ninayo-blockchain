a9e755b835cf04a54b976b5aae31f715b93b6838
beck <beck.nygard@gmail.com>
2016-11-03 21:05:40 -0700

    update whatsapp id in user params

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -431,6 +431,6 @@ private
 		params.require(:ad).permit(:user, :crop_type_id, :other_crop_type, :description, :price, :volume, :volume_unit, :village, :region_id, :district_id, :ward_id, :position, :status, :negotiable, :lat, :lng, :final_price, :archived_at, :buyer_id, :buyer_price, :rating, :ad_type)
 	end
 	def user_params
-		params.require(:user).permit(:name, :email, :gender, :phone_number, :region_id, :district_id, :ward_id, :village, :birthday)
+		params.require(:user).permit(:name, :email, :gender, :phone_number, :whatsapp_id, :region_id, :district_id, :ward_id, :village, :birthday)
 	end
 end
