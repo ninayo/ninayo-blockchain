21bd5b5a12a950211614377601884e3b5b49f4b8
beck <beck.nygard@gmail.com>
2016-11-25 09:51:02 -0800

    added new text to english instructions

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -13,25 +13,22 @@ class InvitesController < ApplicationController
   def create
     @invite = Invite.new(invite_params)
     @invite.sender_id = current_user.id
-    @user = current_user
 
     if @invite.save
-
-      if @invite.recipient != nil
-        #inviting someone who's already in the system
-        #do something, maybe send a reminder email
-        #InviteMailer.existing_user_invite(@invite).deliver
+      if !@invite.recipient.nil?
+        # inviting someone who's already in the system
+        # do something, maybe send a reminder email
+        # InviteMailer.existing_user_invite(@invite).deliver
       else
-        #new user, send them the standard invite with a new token
-        InviteMailer.new_user_invite(@invite, @user, new_user_registration_path(:invite_token => @invite.token)).deliver
+        # new user, send them the standard invite with a new token
+        InviteMailer.new_user_invite(@invite, current_user, new_user_registration_path(invite_token: @invite.token)).deliver
       end
-      #it worked, go home
-      redirect_to root_url, notice: "Mwaliko kutumwa. Asante!"
+      # it worked, go home
+      redirect_to root_url, notice: 'Mwaliko kutumwa. Asante!'
     else
-      #creating an invite failed for some reason
-      redirect_to root_url, notice: "Huwezi kutuma ujumbe"
+      # creating an invite failed for some reason
+      redirect_to root_url, notice: 'Huwezi kutuma ujumbe'
     end
-
   end
 
   private
@@ -43,5 +40,4 @@ class InvitesController < ApplicationController
   def invite_params
     params.require(:invite).permit(:email)
   end
-
 end
