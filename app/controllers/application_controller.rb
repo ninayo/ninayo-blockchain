9e65bfafe4f411261df06b458f976d000dfbfd02
beck <beck.nygard@gmail.com>
2016-12-29 09:39:54 -0800

    added login gate to card layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,72 +1,59 @@
 class ApplicationController < ActionController::Base
-	# Prevent CSRF attacks by raising an exception.
-	# For APIs, you may want to use :null_session instead.
-	before_action :set_locale
-	protect_from_forgery with: :exception
-	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :store_location
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  before_action :set_locale
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_location
 
-	# def set_locale
-	# 	#I18n.locale = params[:locale] || I18n.default_locale
-	# 	I18n.locale = :en
-	# 	#I18n.locale = current_user.language if current_user
-	# end
+  # this and redirect_back_or are required for mailboxer, in case of no messages
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:warning] = 'Resource not found'
+    redirect_back_or root_path
+  end
 
+  def redirect_back_or(path)
+    redirect_to request.referer || path
+  end
 
-	#both this and redirect_back_or are required for mailboxer, in case a user has no messages
-	rescue_from ActiveRecord::RecordNotFound do
-		flash[:warning] = 'Resource not found'
-		redirect_back_or root_path
-	end
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 
-	def redirect_back_or(path)
-		redirect_to request.referer || path
-	end
+  def after_sign_in_path_for(resource)
+    redirect = session[:previous_url]
+    return redirect unless redirect.nil?
+    root_path
+  end
 
-	def not_found
-		raise ActionController::RoutingError.new('Not Found')
-	end
+  protected
 
-	def after_sign_in_path_for(resource)
-		#root_path
-		redirect = session[:previous_url]
-		return redirect unless redirect.nil?
-		root_path
-	end
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
-	protected
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
-	def set_locale
-		# if current_user
-		# 	I18n.locale = current_user.language
-		# else
-			I18n.locale = params[:locale] || I18n.default_locale
-		# end
-	end
+  def store_location
+    # store last url as long as it isn't a /users path or a .json request
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/ || request.fullpath =~ /\/messages\/new/ || request.fullpath =~ /\/contact-info/ || request.fullpath =~ /\/terms/ || request.fullpath.include?(".json")
+  end
 
-	def default_url_options(options = {})
-		{ locale: I18n.locale }.merge options
-	end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login, :email, :phone_number, :password, :password_confirmation, :remember_me, :name, :phonenumber, :region, :village, :language, :lat, :lng, :agreement) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :phone_number, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :phone_number, :region_id, :village, :language, :lat, :lng, :language, :agreement) }
+  end
 
-	def store_location
-		# store last url as long as it isn't a /users path or a .json request
+  def admin_user?
+    current_user && current_user.admin?
+  end
+  helper_method :admin_user?
 
-		session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/ || request.fullpath =~ /\/messages\/new/ || request.fullpath =~ /\/contact-info/ || request.fullpath =~ /\/terms/ || request.fullpath.include?(".json")
-	end
-
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:login, :email, :phone_number, :password, :password_confirmation, :remember_me, :name, :phonenumber, :region, :village, :language, :lat, :lng, :agreement) }
-		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :phone_number, :password, :remember_me) }
-		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :phone_number, :region_id, :village, :language, :lat, :lng, :language, :agreement) }
-	end
-
-	def admin_user?
-		current_user && current_user.admin?
-	end
-	helper_method :admin_user?
-
-	def vip_user?
-		current_user && (current_user.vip? || current_user.admin?)
-	end
-	helper_method :vip_user?
+  def vip_user?
+    current_user && (current_user.vip? || current_user.admin?)
+  end
+  helper_method :vip_user?
 end
--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,56 +1,55 @@
 class MypageController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def index
-		
-	end
+  def index
+  end
 
-	def favorites
-		# todo: filter out ads the user marked as bought
-		@ads = current_user.favorites
-							.published
-							.where('buyer_id IS null OR buyer_id != ?', current_user.id)
-							.includes(:user)
-							.includes(:crop_type)
-							.page(params[:page])
+  def favorites
+    # TODO: filter out ads the user marked as bought
+    @ads = current_user.favorites
+                       .published
+                       .where('buyer_id IS null OR buyer_id != ?', current_user.id)
+                       .includes(:user)
+                       .includes(:crop_type)
+                       .page(params[:page])
 
-		@bought_ads = Ad.where(:buyer => current_user, :buyer_price => nil)
-						.page(params[:page])
+    @bought_ads = Ad.where(buyer: current_user, buyer_price: nil)
+                    .page(params[:page])
 
-		@view = "favorites"
+    @view = 'favorites'
 
-		respond_to do |format|
-			format.html
-		end
-	end
+    respond_to do |format|
+      format.html
+    end
+  end
 
-	def current
-		#@ads = current_user.ads.where(:status => "published")
-		@ads = current_user.ads
-							.published
-							.includes(:crop_type)
-							.page(params[:page])
-		@view = "current"
+  def current
+    @ads = current_user.ads
+                       .published
+                       .includes(:crop_type)
+                       .page(params[:page])
+    @view = 'current'
 
-		respond_to do |format|
-			format.html # index.html.erb
-		end
-	end
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
 
-	def archive
-		@ads = current_user.ads
-							.archived.includes(:user, :buyer, :crop_type)
-							.page(params[:page])
+  def archive
+    @ads = current_user.ads
+                       .archived
+                       .includes(:user, :buyer, :crop_type)
+                       .page(params[:page])
 
-		@bought_ads = Ad.where(:buyer => current_user)
-						.where.not(:archived_at => nil)
-						.includes(:user)
-						.page(params[:page])
+    @bought_ads = Ad.where(buyer: current_user)
+                    .where.not(archived_at: nil)
+                    .includes(:user)
+                    .page(params[:page])
 
-		@view = "archived"
+    @view = 'archived'
 
-		respond_to do |format|
-			format.html
-		end
-	end
+    respond_to do |format|
+      format.html
+    end
+  end
 end
