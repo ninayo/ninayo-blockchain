628569621f4f2476b7bda6a57bbd404f318b28f2
beck <beck.nygard@gmail.com>
2017-03-10 17:48:43 -0800

    prices frontend views, mods to prices controller

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,4 +1,55 @@
 # Controller for pricing pages
 class PricesController < ApplicationController
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def current
+    @prices = Price.all
+                       .published
+                       .includes(:crop_type)
+                       .page(params[:page])
+    @view = 'current'
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def dar_view
+    @prices = Price.where('region_id IS 2')
+                    .includes(:crop_type)
+                    .page(params[:page])
+
+    @view = 'dar'
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def iringa_view
+    @prices = Price.where('region_id IS 5')
+                    .includes(:crop_type)
+                    .page(params[:page])
+
+    @view = 'iringa'
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def mbeya_view
+    @prices = Price.where('region_id IS 13')
+                    .includes(:crop_type)
+                    .page(params[:page])
+
+    @view = 'mbeya'
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
 end
