1aef526e46b1f4927d65ffca567bb3e92ed3a7aa
beck <beck.nygard@gmail.com>
2017-03-13 18:04:35 -0700

    singular new price route

--- config/routes.rb
+++ config/routes.rb
@@ -41,10 +41,6 @@ Rails.application.routes.draw do
       resources :comments
     end
 
-    resources :prices do
-      resources :prices
-    end
-
     resources :conversations, only: [:index, :show, :destroy] do
       member do
         post :reply
@@ -88,6 +84,7 @@ Rails.application.routes.draw do
     get 'prices/dar' => 'prices#dar', as: :dar_price
     get 'prices/mbeya' => 'prices#mbeya', as: :mbeya_price
     get 'prices/iringa' => 'prices#iringa', as: :iringa_price
+    get 'prices/new' => 'prices#new', as: :new_price
 
     #devise_for :users, :controllers => { :sessions => "track_sessions" }
     # The priority is based upon order of creation: first created -> highest priority.
