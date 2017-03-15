6b8f7f0e9a8194e3ed947837dbbec51869301922
beck <beck.nygard@gmail.com>
2017-03-15 13:03:45 -0700

    validations on prices accepted

--- app/models/price.rb
+++ app/models/price.rb
@@ -6,6 +6,8 @@ class Price < ActiveRecord::Base
   belongs_to :region
 
   validates :region_id, :crop_type, :price, presence: true
+  validates :price, numericality: { less_than_or_equal_to: 999_999_999 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 
   def adjust_price
     self.price = self.price.sub!(',', '.') if self.price && self.price.is_a?(String) && self.price.count(',') > 0
