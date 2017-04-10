79787543e8f8f2b2a52ae3a6558655d18bd88345
Beck <beck.nygard@gmail.com>
2017-04-10 12:48:43 -0700

    build out basic text controller

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,2 +1,18 @@
 class TextMessagesController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+
+    def create
+        TextMessage.create(message_attributes)
+        head :ok #need to have this or we run into errors with twilio api
+    end
+
+    private
+
+    def message_attributes
+        {   to: params[:To],
+            from: params[:From],
+            body: params[:Body]
+        }
+
 end
