fec08c562ec3041aca8158e306ad8a2bf2b17ba8
Beck <beck.nygard@gmail.com>
2017-04-27 11:58:26 -0700

    time constraint

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -111,6 +111,7 @@ class Ad < ActiveRecord::Base
   def related_ads
     if self.ad_type == "sell"
       return Ad.where(:ad_type => 1, 
+                       "created_at >= ? OR updated_at >= ?", 2.weeks.ago,
                       :crop_type_id => self.crop_type_id, 
                       :volume_unit => Ad.volume_units[self.volume_unit],
                       :region_id => self.region_id).order("published_at").last(3).reverse
