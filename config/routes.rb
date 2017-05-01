676fdb8aed65582d345bc490d75ba611c992d7f6
Beck <beck.nygard@gmail.com>
2017-05-01 16:20:18 -0700

    password reset frontend

--- config/routes.rb
+++ config/routes.rb
@@ -101,6 +101,8 @@ Rails.application.routes.draw do
 
   post 'text', to: 'text_messages#create'
 
+  patch 'sms_reset/:phone_number' => 'registrations#find_for_sms_reset', as: :sms_reset 
+
   get '/.well-known/acme-challenge/-psVTriG2kn-BOKuC52nnPuie6YQ9L8-6yd97DlA78g' => 'splash#letsencrypt_verify'
 
   get '/:locale' => 'splash#index'
