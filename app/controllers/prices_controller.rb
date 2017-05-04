95fd45ad00045078c4ce68221526ebe78596eba3
beck <beck.nygard@gmail.com>
2017-05-04 15:18:07 -0700

    sort_by rather than order

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -127,7 +127,7 @@ class PricesController < ApplicationController
 
   def extract_lowest_price(ads)
     return 0 if ads.empty?
-    ads.order(:price).reverse.first
+    ads.sort_by{ |a| a.price }.reverse.first
   end
 
   def price_params(params_hash)
