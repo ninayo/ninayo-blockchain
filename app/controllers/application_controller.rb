b1f8a10dd70e545ec43dc3e9f25a2dc02ed2c894
beck <beck.nygard@gmail.com>
2017-05-17 15:25:28 -0700

    request ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,7 +71,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_ip_address
-    request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR'] || request.ip
+    request.ip || request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
   end
 
   def ip_range(first_three)
