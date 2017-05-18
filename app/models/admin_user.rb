1d8d2326e5fe7fac4ca62c7116924eceac09fcb8
beck <beck.nygard@gmail.com>
2017-05-18 13:51:41 -0700

    build out admin user model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,14 +1,20 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+
+  # setup accessible attributes for parity with normal user model
+  attr_accessible :email, :login, :password,
+                  :password_confirmation, :remember_me
+
+  attr_accessor :login
+
+  protected
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(email) = :value", { value: login.strip.downcase }]).first
+  end
 end
