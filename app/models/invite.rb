de92765dfd4bba174b1eee67bf5f2ceaee76139a
beck <beck.nygard@gmail.com>
2017-05-19 14:13:49 -0700

    refactor on invite model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,21 +1,18 @@
+# works but doesn't get used at the moment
 class Invite < ActiveRecord::Base
-
   before_create :generate_token
 
-  #before_save :check_user_existence
+  # before_save :check_user_existence
 
-  belongs_to :sender, :class_name => "User"
-  belongs_to :recipient, :class_name => "User"
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
 
   def check_user_existence
     recipient = User.find_by_email(email)
-    if recipient
-      self.recipient_id = recipient.id
-    end
+    self.recipient_id = recipient.id if recipient
   end
 
   def generate_token
     self.token = SecureRandom.urlsafe_base64(16)
   end
-
 end
