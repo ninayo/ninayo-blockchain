263d118aa8b41bd5ddb9f0f2208d8ead06e72a0c
beck <beck.nygard@gmail.com>
2017-05-19 13:02:54 -0700

    validate phone number length on help request model

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,5 +1,6 @@
 class HelpRequest < ActiveRecord::Base
   validates :request_type, :body, :phone_number, presence: true, on: :create
   validates :phone_number, numericality: true
+  validates :phone_number, length: { is: 10 }
   belongs_to :user
 end
