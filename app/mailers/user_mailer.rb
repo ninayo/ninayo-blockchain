06aa351a71cd46a004eb7a6779523deb5e7ea662
beck <beck.nygard@gmail.com>
2017-05-23 10:56:55 -0700

    email on new help request

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,19 @@
 class UserMailer < ApplicationMailer
-	default from: "info@ninayo.com"
+  default from: 'info@ninayo.com'
 
-	# Subject can be set in your I18n file at config/locales/en.yml
-	# with the following lookup:
-	#
-	#   en.user_mailer.signup_confirmation.subject
-	#
-	def signup_confirmation(user)
-		@user = user
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup_confirmation.subject
+  #
+  def signup_confirmation(user)
+    @user = user
 
-		mail to: user.email
-	end
+    mail to: user.email
+  end
+
+  def help_request(request)
+    @request = request
+    mail to: 'beck@ninayo.com'
+  end
 end
--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -5,5 +5,11 @@ class HelpRequest < ActiveRecord::Base
   validates :phone_number, length: { is: 10 }
   belongs_to :user
 
+  after_create :send_notification_email
+
   enum request_type: [:seller_help, :buyer_help, :general_help, :report_problem]
+
+  def send_notification_email
+    UserMailer.help_request(self).deliver
+  end
 end
