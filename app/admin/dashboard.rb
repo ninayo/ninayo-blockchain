d4e9f01ae7bc33759fe810f674500a81f9b99d2a
beck <beck.nygard@gmail.com>
2017-05-24 15:07:42 -0700

    wrong end

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
+  content title: proc{ I18n.t("active_admin.dashboard") } do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -34,5 +35,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-    # content
+  end # content
 end
