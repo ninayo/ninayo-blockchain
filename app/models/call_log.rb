c84e087808b8fa30c390b7067d91b5db73436d9a
beck <beck.nygard@gmail.com>
2017-05-31 10:48:32 -0700

    move to has_one association for call and text logs

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -2,5 +2,5 @@ class CallLog < ActiveRecord::Base
   belongs_to :caller, class_name: 'User', foreign_key: 'caller_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
   belongs_to :ad
-  delegate :region, to: :ad, allow_nil: true
+  has_one :region, through: :ad, autosave: false
 end
