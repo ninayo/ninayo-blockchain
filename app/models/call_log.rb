f56c7d0bc8452ee1d2f82bb401b3ac8199943a1d
beck <beck.nygard@gmail.com>
2017-05-31 10:30:02 -0700

    added through associations to call and text models

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -3,5 +3,7 @@ class CallLog < ActiveRecord::Base
   belongs_to :caller, :class_name => "User", :foreign_key => "caller_id"
   belongs_to :receiver, :class_name => "User", :foreign_key => "receiver_id"
   belongs_to :ad
+  belongs_to :region, through: :ad
+  belongs_to :district, through: :ad
 
 end
--- app/models/text_log.rb
+++ app/models/text_log.rb
@@ -3,5 +3,7 @@ class TextLog < ActiveRecord::Base
   belongs_to :sender, :class_name => "User", :foreign_key => "sender_id"
   belongs_to :receiver, :class_name => "User", :foreign_key => "receiver_id"
   belongs_to :ad
+  belongs_to :region, through: :ad
+  belongs_to :district, through: :ad
 
 end
