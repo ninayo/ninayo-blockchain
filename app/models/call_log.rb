5fa5780b3f5f896bb970afec93edf87dcd9c03f0
beck <beck.nygard@gmail.com>
2017-05-31 10:34:29 -0700

    use delegate method rather than belongs_to through

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -1,9 +1,6 @@
 class CallLog < ActiveRecord::Base
-
-  belongs_to :caller, :class_name => "User", :foreign_key => "caller_id"
-  belongs_to :receiver, :class_name => "User", :foreign_key => "receiver_id"
+  belongs_to :caller, class_name: 'User', foreign_key: 'caller_id'
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
   belongs_to :ad
-  belongs_to :region, through: :ad
-  belongs_to :district, through: :ad
-
+  delegate :region, to: :ad, allow_nil: true
 end
--- app/models/text_log.rb
+++ app/models/text_log.rb
@@ -1,9 +1,6 @@
 class TextLog < ActiveRecord::Base
-
-  belongs_to :sender, :class_name => "User", :foreign_key => "sender_id"
-  belongs_to :receiver, :class_name => "User", :foreign_key => "receiver_id"
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
   belongs_to :ad
-  belongs_to :region, through: :ad
-  belongs_to :district, through: :ad
-
+  delegate :region, to: :ad, allow_nil: true
 end
