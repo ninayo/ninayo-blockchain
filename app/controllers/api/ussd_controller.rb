8bfcc2d6cdbdd089eac11e697629dba4182116f8
beck <beck.nygard@gmail.com>
2017-05-31 14:17:48 -0700

    refactor on ussd controller

--- app/controllers/api/ussd_controller.rb
+++ app/controllers/api/ussd_controller.rb
@@ -1,22 +1,20 @@
-#this is documented with req/res examples at http://docs.ninayoussd.apiary.io/#
-
+# this is documented with req/res examples at http://docs.ninayoussd.apiary.io/#
 class Api::UssdController < ApplicationController
-
   skip_before_filter :verify_authenticity_token
   before_action :find_user_by_phone, :restrict_access
 
   respond_to :json
 
   def post_ad
-    #a little more information needed, we need to figure out location stuff
+    # a little more information needed, we need to figure out location stuff
 
-    @ad = Ad.new(params.permit(:price, :volume, :volume_unit, :crop_type_id, :region_id, :village, :lat, :lng))
+    @ad = Ad.new(ad_params)
     @ad.user = @user
     @ad.ad_type, @ad.status = 0, 1
     if @ad.save!
-      render json: {:status => 201}
+      render json: { status: 201 }
     else
-      render json: {:status => 422}
+      render json: { status: 422 }
     end
   end
 
@@ -26,7 +24,7 @@ class Api::UssdController < ApplicationController
     if @ad.nil?
       respond_with status: 404
     elsif @user.ads.include?(@ad)
-      if @ad.update!(:status => 2, :archived_at => Time.now, :final_price => @ad.price)
+      if @ad.update(status: 2, archived_at: Time.now, final_price: @ad.price)
         respond_with status: 202
       else
         respond_with status: 422
@@ -36,9 +34,9 @@ class Api::UssdController < ApplicationController
     end
   end
 
-  #calls db query, returns hash with keys named after db cols
-  #modify keys arr based on what we need/want
-  def get_ads_for_user
+  # calls db query, returns hash with keys named after db cols
+  # modify keys arr based on what we need/want
+  def ads_for_user
     if @user.nil?
       respond_with status: 404
     else
@@ -62,24 +60,32 @@ class Api::UssdController < ApplicationController
     respond_with(@regions, status: 200)
   end
 
-  #takes AR collection and array of db cols, returns json object with K/Vs
+  # takes AR collection and array of db cols, returns json object with K/Vs
   def pluck_to_json(relation, key_array)
     relation.pluck(*key_array)
-            .map{ |pa| Hash[key_array.zip(pa)] }
+            .map { |pa| Hash[key_array.zip(pa)] }
             .to_json
   end
 
   def find_user_by_phone
-    #might want to use find_by! here since it returns RecordNotFound error
-    @user ||= User.find_by(:phone_number => params[:phone])
+    # might want to use find_by! here since it returns RecordNotFound error
+    @user ||= User.find_by(phone_number: params[:phone])
   end
 
-  #requires token to be sent in header
-  #> curl ninayo.com/en/api/etc -H 'Access: Token token=cfd5f147ef3140d8ce2a3e207b129aac'
+  # requires token to be sent in header
+  # > curl ninayo.com/en/api/etc -H 'Access: Token token=cfd5f147ef3140d8ce2a3e207b129aac'
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
       ApiKey.exists?(access_token: token)
     end
   end
 
+  def ad_params
+    params.require(:ad).permit(:price,
+                               :volume,
+                               :volume_unit,
+                               :crop_type_id,
+                               :region_id,
+                               :village)
+  end
 end
