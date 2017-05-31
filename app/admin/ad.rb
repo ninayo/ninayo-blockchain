8ce03ad674031c01d1ca64dbd0187e696f075de9
beck <beck.nygard@gmail.com>
2017-05-31 10:19:26 -0700

    remove big comment blocks for admin models

--- app/admin/ad.rb
+++ app/admin/ad.rb
@@ -1,17 +1,4 @@
 ActiveAdmin.register Ad do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
   permit_params :crop_type, :region_id, :district_id, :ward_id, :village,
                 :price, :volume, :volume_unit, :published_at, :archived_at
 
