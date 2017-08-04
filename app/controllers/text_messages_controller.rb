6044c8a43b7472cb677c936d87c50b6d7c92a672
Beck <beck.nygard@gmail.com>
2017-08-03 21:13:00 -0700

    district zero for time being, todo

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -162,6 +162,7 @@ class TextMessagesController < ApplicationController
     #find the region
 
     @new_sms_ad.region_id = Region.find_by(name: best_match(message_contents[4].titleize, Region.all.map(&:name))).id
+    @new_sms_ad.district_id = 0
 
   end
 
